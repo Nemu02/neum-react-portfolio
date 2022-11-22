515ed7a3e77da7797b01acad25a97bde0bdc4677
Nemu02 <neumv2@icloud.com>
2022-11-21 18:00:04 -0600

    test 2

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import PortfolioItem from "./portfolio-item";
 
 import PortfolIoItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
     constructor() {
-        super();
+        super()
         console.log("Portfolio Container rendered");
+    }
+
+    portfolioItems() {
+        const data = ["Diner Menu", "Nav BAr CSS", "UML"];
+
+        return data.map(item => {
+            return <PortfolioItem title={item} />;
+        })
     }
 
     render() {
@@ -13,7 +22,7 @@ export default class PortfolioContainer extends Component {
             <div>
                 <h2>Updated Items Goes HERE!</h2>
 
-                <PortfolIoItem />
+                {this.portfolioItems()}
             </div>
         )
     }
